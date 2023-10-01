c444b205b6da131ac5b7b07ced058522b966dc99
bemuse01 <sequeland@gmail.com>
2023-10-01 15:07:23 +0900

    update

---
+++ assets/js/class/commands/ls.js
@@ -0,0 +1,75 @@
+import Method from '../../method/method.js'
+
+export default class{
+    constructor(){
+        this.dirs = ['home', 'desktop', 'temp', 'backups', 'docs', 'systems', 'scripts', 'logs', 'downloads']
+        this.type = ['d']
+        this.auth = ['r', 'w', 'x']
+        this.months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        this.chars = 'abcdefghijklmnopqrstuvwxyz-_'
+        this.numsOfDay = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    }
+
+
+    // command
+    createCommand(){
+        const main = 'ls'
+        const dir1 = this.dirs[~~(Math.random() * this.dirs.length)]
+        const dir2 = Method.uuidv4()
+        return `${main} -l /${dir1}/${dir2}`
+    }
+
+
+    // info
+    createInfos(num){
+        const temp = Array.from({length: num}, _ => this.createInfo())
+        temp.unshift('')
+        temp.push('')
+        return temp
+    }
+    createInfo(){
+        const auth = this.createAuths()
+        const hardlink = this.createHardLink()
+        const owner = 'user user'
+        const size = this.createSize()
+        const date = this.createDate()
+        const name = this.createName()
+        return [' ', auth, hardlink, owner, size, date, name].join('  ')
+    }
+    // auth
+    createAuths(){
+        const type = this.createAuth(this.type)
+        const owner = this.createAuth(this.auth)
+        const group = this.createAuth(this.auth)
+        const other = this.createAuth(this.auth)
+        return type + owner + group + other
+    }
+    createAuth(temp){
+        return temp.map(e => Math.random() < 0.5 ? e : '-').join('')
+    }
+    // hard link
+    createHardLink(){
+        return ~~(Math.random() * 9 + 1)
+    }
+    // size
+    createSize(){
+        return 2 ** ~~(Math.random() * 17)
+    }
+    // date
+    createDate(){
+        const idx = ~~(Math.random() * this.months.length)
+        const month = this.months[idx]
+        const day = ~~(Math.random() * this.numsOfDay[idx])
+        const rhour = ~~(Math.random() * 24)
+        const hour = rhour < 10 ? rhour + '0' : rhour + ''
+        const rmin = ~~(Math.random() * 60)
+        const min = rmin < 10 ? rmin + '0' : rmin + ''
+        const time = `${hour}:${min}`
+        return `${month} ${day} ${time}`
+    }
+    // name
+    createName(){
+        const len = ~~(Math.random() * 15 + 3)
+        return Array.from({length: len}, _ => this.chars[~~(Math.random() * this.chars.length)]).join('')
+    }
+}
--- assets/js/component/canvas/textBox.js
+++ assets/js/component/canvas/textBox.js
@@ -1,3 +1,5 @@
+import LS from '../../class/commands/ls.js'
+
 export default {
     props: {
         flex: String,
@@ -7,9 +9,18 @@ export default {
             :ref="el => box = el"
             :style="boxStyle"
         >
-            <canvas 
-                :ref="el => canvas = el"
-            />
+
+            <div
+                :ref="el => wrapper = el"
+                :style="wrapperStyle"
+            >
+
+                <canvas 
+                    :ref="el => canvas = el"
+                />
+
+            </div>
+            
         </div>
     `,
     setup(props){
@@ -23,16 +34,26 @@ export default {
         // box
         const box = ref()
         const boxStyle = computed(() => ({
-            flex
+            flex,
+            paddingBottom: '20px'
+        }))
+
+
+        // wrapper
+        const wrapper = ref()
+        const wrapperStyle = computed(() => ({
+            width: '100%',
+            height: '100%',
+            display: 'flex'
         }))
 
 
         // canvas
         const canvas = ref()
         const ctx = ref()
-        const fontSize = 12
+        const fontSize = 14
         const initCanvas = () => {
-            const {width, height} = box.value.getBoundingClientRect()
+            const {width, height} = wrapper.value.getBoundingClientRect()
 
             canvas.value.width = width
             canvas.value.height = height
@@ -42,7 +63,7 @@ export default {
             ctx.value.fillStyle = '#ffffff'
         }
         const resizeCanvas = () => {
-            const {width, height} = box.value.getBoundingClientRect()
+            const {width, height} = wrapper.value.getBoundingClientRect()
 
             canvas.value.width = width
             canvas.value.height = height
@@ -56,41 +77,75 @@ export default {
         const hex = '0123456789abcedf'
         const textCols = 20
         const logs = []
-        const intervalTime = 200
+        const intervalTime = 20
         let startTime = 0
-        const lineStep = 2
+        const prompt = 'user@main-host: ~$ '
+        const ls = new LS()
+
+        let currentCommand = ls.createCommand()
+        let cmdIdx = 0
         const randHex = () => hex[~~(Math.random() * hex.length)]
         const generateText = () => {
             return Array.from({length: textCols}, _ => randHex() + randHex()).join('  ')
         }
+        const generateCommand = () => {
+            // const command = commands[~~(Math.random() * commands.length)]
+            const main = 'ls'
+
+            switch(main){
+                case 'ls':
+                    const num = ~~(Math.random() * 5 + 1)
+                    const newCommand = ls.createCommand()
+
+                    logs.push(prompt + currentCommand)
+                    logs.push(...ls.createInfos(num))
+
+                    return newCommand
+                default:
+                    return
+            }
+        }
         const drawText = () => {
             if(!ctx.value) return
 
-            const {width, height} = box.value.getBoundingClientRect()
+            const {width, height} = wrapper.value.getBoundingClientRect()
 
             ctx.value.clearRect(0, 0, width, height)
-            
-            const text = generateText()
-            logs.push({text, step: 1})
 
-            logs.forEach(log => {
-                const {text, step} = log
+
+            // logging 
+            logs.forEach((log, idx) => {
                 const x = 0
-                const y = height - fontSize * step * lineStep
-                log.step++
+                const y = height - fontSize * (logs.length - idx)
 
-                ctx.value.fillText(text, x, y)
+                ctx.value.fillText(log, x, y)
             })
 
-            if(logs.length > Math.ceil(height / (fontSize * lineStep))){
-                logs.shift()
-            }            
+
+            // create new command
+            if(cmdIdx >= currentCommand.length){
+                currentCommand = generateCommand()
+                cmdIdx = 0
+            }
+
+
+            // remove logs out of canvas
+            if(logs.length > Math.ceil(height / fontSize)){
+                logs.splice(0, logs.length - Math.ceil(height / fontSize))
+                // console.log(logs)
+            }
+
+
+            // write command
+            const command = prompt + currentCommand.slice(0, cmdIdx++)
+            ctx.value.fillText(command, 0, height)
         }
         const interval = () => {
             const currentTime = window.performance.now()
 
             if(currentTime - startTime > intervalTime){
                 drawText()
+
                 startTime = currentTime
             }
         }
@@ -120,7 +175,9 @@ export default {
         return{
             box,
             boxStyle,
-            canvas
+            wrapper,
+            wrapperStyle,
+            canvas,
         }
     }
 }
